4778d5a7b5e8ce853b97a92dac0a88a18a4a3b98
Janssen125 <janssenaddisonchen@gmail.com>
2024-11-15 17:35:52 +0700

    5 minutes delay ahh timestamp
    i edge on this

--- static/js/chat.js
+++ static/js/chat.js
@@ -30,9 +30,9 @@ function scrollToBottom() {
   }
 }
 
-function convertToGMTPlus7(utcDateString) {
-  const date = new Date(utcDateString);
-  const gmtPlus7Offset = 7 * 60 * 60 * 1000;
-  const gmtPlus7Date = new Date(date.getTime() + gmtPlus7Offset);
-  return gmtPlus7Date.toLocaleTimeString(); 
-  }
+// function convertToGMTPlus7(utcDateString) {
+//   const date = new Date(utcDateString);
+//   const gmtPlus7Offset = 7 * 60 * 60 * 1000;
+//   const gmtPlus7Date = new Date(date.getTime() + gmtPlus7Offset);
+//   return gmtPlus7Date.toLocaleTimeString(); 
+//   }
